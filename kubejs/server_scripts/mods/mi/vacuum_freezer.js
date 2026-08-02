b231c050520e37bdfa038704f65a834a36878a4f
caco3-111 <1528170107@qq.com>
2026-08-02 22:21:51 +0800

    6

---
+++ kubejs/server_scripts/mods/mi/vacuum_freezer.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(event => {
+event.recipes.modern_industrialization.vacuum_freezer(128, 250)
+.itemIn('modern_industrialization:silicon_battery')
+.itemIn('modern_industrialization:sodium_battery')
+.itemOut('ccb:frozen_battery_stick_ice')
+.fluidIn("8000x irons_spellbooks:ice_venom")
+.fluidIn("1000x modern_industrialization:cryofluid")
+})
