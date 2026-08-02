6806e802e30fd79fe668813b2299fa7b34241f9a
KuDikan <1302147038@qq.com>
2026-08-02 13:29:50 +0800

    给人造陨星添加多方块处理阵列的黑名单tag

--- kubejs/server_scripts/mods/mi/artificial_star.js
+++ kubejs/server_scripts/mods/mi/artificial_star.js
@@ -1,7 +1,11 @@
 ServerEvents.recipes(event => {
-event.recipes.modern_industrialization.artificial_star(0, 100)
-.itemIn('mekanism:pellet_antimatter')
-.itemIn('mekmm:uu_matter')
-.itemOut('draconicevolution:chaos_shard')
-.euOut(76800000000)
-})
+    event.recipes.modern_industrialization.artificial_star(1, 100)
+        .itemIn('mekanism:pellet_antimatter')
+        .itemIn('mekmm:uu_matter')
+        .itemOut('draconicevolution:chaos_shard')
+        .euOut(76800000000);
+});
+
+ServerEvents.tags('item', event => {
+    event.add('industrialization_overdrive:multi_processing_array_blacklist', 'modern_industrialization:artificial_star');
+});
