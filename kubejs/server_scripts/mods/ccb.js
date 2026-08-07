b465050d60f556ff6ec8a719f6406cde97e4e566
caco3-111 <1528170107@qq.com>
2026-08-07 13:38:42 +0800

    补完食物配方 注册了一些数据模型

--- kubejs/server_scripts/mods/ccb.js
+++ kubejs/server_scripts/mods/ccb.js
@@ -2,4 +2,14 @@ ServerEvents.recipes(event => {
     event.remove({ output: 'ccb_core:natural_lightning_spawner' });
     event.smithing('ccb_core:natural_lightning_spawner',
         'irons_spellbooks:lightning_bottle', 'minecraft:trident', 'powah:charged_snowball');
+    event.shaped('ccb:mi_prediction_matrix',[
+    "AB ",
+    "BCB",
+    " BD"
+],{
+    A:'immersiveengineering:ingot_aluminum',
+    B:'immersiveengineering:slag_glass',
+    C:'hostilenetworks:prediction_matrix',
+    D:'minecraft:copper_ingot'
+})
 });
