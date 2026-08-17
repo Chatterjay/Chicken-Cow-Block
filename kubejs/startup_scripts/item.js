b1ffaf2ab70d978571f0447e9b305b9989d1132a
caco3-111 <1528170107@qq.com>
2026-08-17 22:51:21 +0800

    完善了真结局的物品注册

--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -67,4 +67,75 @@ StartupEvents.registry("item", event => {
                 .append(Text.lightPurple(Text.literal('影')))
         )
         .tooltip(Text.translate('tooltip.ccb.broken_world_fragment').gray());
+
+    // 充盈世界的食物
+    event.create('ccb:world_filling_food')
+        .formattedDisplayName(Text.translate('item.ccb.world_filling_food').gold())
+        .tooltip(Text.translate('tooltip.ccb.world_filling_food').gray());
+
+    // 归墟盏
+    event.create('ccb:guixu_chalice')
+        .formattedDisplayName(Text.translate('item.ccb.guixu_chalice').aqua())
+        .tooltip(Text.translate('tooltip.ccb.guixu_chalice').gray());
+
+    // 固道箓
+    event.create('ccb:gudao_talisman')
+        .formattedDisplayName(Text.translate('item.ccb.gudao_talisman').blue())
+        .tooltip(Text.translate('tooltip.ccb.gudao_talisman').gray());
+
+    // 寰宇恒核
+    event.create('ccb:cosmos_core')
+        .formattedDisplayName(Text.translate('item.ccb.cosmos_core').white())
+        .tooltip(Text.translate('tooltip.ccb.cosmos_core').gray());
+
+    // 曜辰玑
+    event.create('ccb:starlight_orb')
+        .formattedDisplayName(Text.translate('item.ccb.starlight_orb').lightPurple())
+        .tooltip(Text.translate('tooltip.ccb.starlight_orb').gray());
+
+    // 衡垠圭
+    event.create('ccb:hengyin_gui')
+        .formattedDisplayName(Text.translate('item.ccb.hengyin_gui').darkPurple())
+        .tooltip(Text.translate('tooltip.ccb.hengyin_gui').gray());
+
+    // 春秋蝉
+    event.create('ccb:chunqiu_cicada')
+        .formattedDisplayName(Text.translate('item.ccb.chunqiu_cicada').gold())
+        .tooltip(Text.translate('tooltip.ccb.chunqiu_cicada').gray());
+
+    // 玄睆璋
+    event.create('ccb:xuanhuang_zhang')
+        .formattedDisplayName(Text.translate('item.ccb.xuanhuang_zhang').darkPurple())
+        .tooltip(Text.translate('tooltip.ccb.xuanhuang_zhang').gray());
+
+    // 纪元璿
+    event.create('ccb:epoch_orb')
+        .formattedDisplayName(Text.translate('item.ccb.epoch_orb').aqua())
+        .tooltip(Text.translate('tooltip.ccb.epoch_orb').gray());
+
+    // 生生圭
+    event.create('ccb:shengsheng_gu')
+        .formattedDisplayName(Text.translate('item.ccb.shengsheng_gu').green())
+        .tooltip(Text.translate('tooltip.ccb.shengsheng_gu').gray());
+
+    // KuDikan
+    event.create('ccb:kudikan')
+        .formattedDisplayName(
+            Text.red(Text.literal('K'))
+                .append(Text.gold(Text.literal('u')))
+                .append(Text.yellow(Text.literal('D')))
+                .append(Text.green(Text.literal('i')))
+                .append(Text.aqua(Text.literal('k')))
+                .append(Text.blue(Text.literal('a')))
+                .append(Text.lightPurple(Text.literal('n')))
+        )
+        .tooltip(Text.translate('tooltip.ccb.kudikan').gray());
+
+    // 草神
+    event.create('ccb:grass_god')
+        .formattedDisplayName(
+            Text.green(Text.literal('草'))
+                .append(Text.gold(Text.literal('神')))
+        )
+        .tooltip(Text.translate('tooltip.ccb.grass_god').gray());
 });
